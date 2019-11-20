60c6072b3d430489c6414ea5d29230e4be0481b5
Joey880627 <47892850+Joey880627@users.noreply.github.com>
2019-11-21 01:55:59 +0800

    Update 3-train.sh

--- 3-train.sh
+++ 3-train.sh
@@ -4,13 +4,13 @@ dir=exp/mono
 feat=feat/train.39.cmvn.ark
 
 ### parameters that you can modify
-numiters=5                                    # Number of iterations of training
-maxiterinc=4                                  # Last iter to increase #Gauss on.
-numgauss=1                                    # Initial num-Gauss (must be more than #states=3*phones).
-totgauss=5                                    # Target #Gaussians.
-incgauss=$[($totgauss-$numgauss)/$maxiterinc] # per-iter increment for #Gauss
-realign_iters="1 2 3 4 5";
-scale_opts="--transition-scale=1.0 --acoustic-scale=0.1 --self-loop-scale=0.1"
+numiters=10                                    # Number of iterations of training
+maxiterinc=20                                  # Last iter to increase #Gauss on.
+numgauss=3000                                  # Initial num-Gauss (must be more than #states=3*phones).
+totgauss=3000                                  # Target #Gaussians.
+incgauss=$[($totgauss-$numgauss)/$maxiterinc]  # per-iter increment for #Gauss
+realign_iters="1 2 3 4 5 6 7 8 9 10";
+scale_opts="--transition-scale=10.0 --acoustic-scale=0.1 --self-loop-scale=0.1"
 ###
 
 mkdir -p $dir
